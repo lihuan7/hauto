ee0f5fca67a8850efab47c77f070375ec36b3bd3
lihuan7 <serg_nikitin@mail.ru>
2016-09-07 12:48:18 +0500

    add hibernate dependency

---
+++ src/main/java/com/chronomaster/AppUser.java
@@ -0,0 +1,45 @@
+package com.chronomaster;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * Created by snikitin on 07.09.16.
+ */
+@Entity
+public class AppUser implements Serializable {
+
+    public AppUser() {
+
+    };
+
+    public AppUser(String login) {
+        this.login = login;
+
+    };
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
---
+++ src/test/java/com/chronomaster/AppTest.java
@@ -0,0 +1,27 @@
+package com.chronomaster;
+
+import junit.framework.TestCase;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by snikitin on 07.09.16.
+ */
+public class AppTest extends TestCase {
+
+    public void testApp() {
+        SessionFactory sessionFactory = new Configuration().configure()
+                .buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        AppUser user = new AppUser("firstuser");
+        session.save(user);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+}
